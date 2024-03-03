58cb4bedaf8552dd99276bfdb97868cb4df0c63c
0xafrogeek <babouceesay@users.noreply.github.com>
2024-03-03 18:47:56 +0000

    refactor: Improve logging for development

--- src/middlewares/error-handler.js
+++ src/middlewares/error-handler.js
@@ -2,5 +2,12 @@ const { logger } = require('../config/dhis2-config')
 
 module.exports = (err, req, res, next) => {
     logger.error(`${req.method} ${req.originalUrl} Error: ${err.message}`)
-    res.status(500).send('An error occurred')
+    if (process.env.NODE_ENV === 'development') {
+        res.status(500).send({
+            message: 'An error occurred:',
+            error: err.stack,
+        })
+    } else {
+        res.status(500).send('An error occured')
+    }
 }
